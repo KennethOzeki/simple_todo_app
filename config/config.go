8038751d03365fbeaa737cba8c5020694cc8016f
KennethOzeki <33027558+KennethOzeki@users.noreply.github.com>
2022-08-16 01:23:23 +0900

    Add second version program 0814 (#2)
    * add: go.mod to main directory.
    
    * fix: config.go init function name typo.
    
    * fix: mv ini file to main dir.
    
    * add: logging utils.
    
    * add: sql model for creating user table.
    
    * add: project read me.

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"log"
 
+	"simple_todo_app/utils"
 	"gopkg.in/go-ini/ini.v1"
 )
 
@@ -17,6 +18,7 @@ var Config ConfigList
 
 func init() {
 	LoadConfig()
+	utils.LoggingSettings(Config.LogFile)
 }
 
 func LoadConfig() {
